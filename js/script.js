3ec00af4d62de4cf6b359ac0625a3fd98d1e2402
PhantomProductions501 <phantomproductions501@proton.me>
2025-07-30 19:54:34 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
+document.querySelectorAll('.gallery-item img').forEach(img => {
+    img.addEventListener('click', () => {
+        document.getElementById('lightbox').style.display = 'flex';
+        document.getElementById('lightbox-img').src = img.src;
+    });
+});
 
+document.querySelector('.close').addEventListener('click', () => {
+    document.getElementById('lightbox').style.display = 'none';
+});
